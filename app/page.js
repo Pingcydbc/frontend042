39d3b9d339f95e5a949857a1f7308c019f46b098
Pingcydbc <pingchayodom2323@gmail.com>
2024-07-04 08:46:37 +0700

    asd

--- app/page.js
+++ app/page.js
@@ -1,34 +1,32 @@
+// test202020
 import Navbar from "../app/component/nav.js";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-{/* asdasdasdadasdadasdasdasdsad */}
-
-      <div class='container'>
-        <div id="carouselExample" class="carousel slide carousel-fade" data-bs-ride="carousel">
-          <div class="carousel-inner">
+    <Navbar />
+    
+    <div class='container'>
+      <div id="carouselExample" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
             <div class="carousel-item active">
-              <img src="http://it.cmtc.ac.th/web2017/photo/upload/2020/1591928290_6033-org.jpg" class="d-block w-100 img-fluid " alt="..."></img>
-              <img src="http://it.cmtc.ac.th/web2017/photo/upload/2020/1591928290_6033-org.jpg" class="d-block w-100 img-fluid " alt="..."></img>
-              <img src="http://it.cmtc.ac.th/web2017/photo/upload/2020/1591928290_6033-org.jpg" class="d-block w-100 img-fluid " alt="..."></img>
-              <img src="http://it.cmtc.ac.th/web2017/photo/upload/2020/1591928290_6033-org.jpg" class="d-block w-100 img-fluid " alt="..."></img>
-              <img src="http://it.cmtc.ac.th/web2017/photo/upload/2020/1591928290_6033-org.jpg" class="d-block w-100 img-fluid " alt="..."></img>
-
+            <img src="http://it.cmtc.ac.th/web2017/photo/upload/2020/1591928290_6033-org.jpg" class="d-block w-100 img-fluid " alt="..."></img>
             </div>
-          </div>
-          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <div class="carousel-item">
+            <img src="img/slide 1.png" class="d-block w-100 img-fluid " alt="..."></img>
+            </div>
+
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
-          </button>
-          <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        </button>
+        <button class="carousel-control-next"asdasd type="button" data-bs-target="#carouselExample" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
-
+        </button>
+    </div>
+    </div>
     </>
   );
 }
